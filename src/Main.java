ad793ca838bf9c7ae5e861eaac32adabd6b45240
Willianb44 <willianbravo53@gmail.com>
2025-04-29 13:25:45 +0200

    Error: borrar contenido principal de Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
-        taskManager.run();
+        //codigo vacio como el alma
     }
 }
